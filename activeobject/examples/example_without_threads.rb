18d8bc72eae54abb6447ec7ce529cb6014ded02b
davidbkemp <davidbkemp@0f78eaf1-5652-0410-a00d-6df5d22118a9>
2008-07-19 05:59:41 +0000

    Added simple example without threads.

---
+++ activeobject/examples/example_without_threads.rb
@@ -0,0 +1,19 @@
+require "src/domain_name_registry"
+
+class DomainNameReseller
+  def initialize
+    @com = DomainNameRegistry.new("com")
+    @com_au = DomainNameRegistry.new("com.au")
+    @co_nz = DomainNameRegistry.new("co.nz")
+  end
+  def buy_across_all_spaces(domain_name)
+    com_result = @com.buy_domain_name(domain_name)
+    com_au_result = @com_au.buy_domain_name(domain_name)
+    co_nz_result = @co_nz.buy_domain_name(domain_name)
+    puts com_result.to_s + ", " + com_au_result.to_s + ", " + co_nz_result.to_s
+  end
+end
+
+
+domain_name_reseller = DomainNameReseller.new
+domain_name_reseller.buy_across_all_spaces("foo")
