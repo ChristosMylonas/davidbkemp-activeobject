85d73f059d864f3fbe1c828c28c8d574fce763df
davidbkemp <davidbkemp@0f78eaf1-5652-0410-a00d-6df5d22118a9>
2008-07-19 05:24:32 +0000

    Moved examples into their own folder, and simplified DomainNameRegistry slightly.

--- trunk/activeobject/src/domain_name_registry.rb
+++ trunk/activeobject/src/domain_name_registry.rb
@@ -10,7 +10,7 @@ class DomainNameRegistry
       return PurchaseResult.new(full_domain_name, false, "Not Available")
     end
     @registry << domain_name
-    return PurchaseResult.new(full_domain_name, true) 
+    return PurchaseResult.new(full_domain_name, true, "Purchased") 
   end
 end
 
@@ -18,12 +18,12 @@ class PurchaseResult
   attr_reader :full_domain_name
   attr_reader :success
   attr_reader :message
-  def initialize(full_domain_name, success, message="")
+  def initialize(full_domain_name, success, message)
     @full_domain_name = full_domain_name
     @success = success
     @message = message
   end
   def to_s
-    return @full_domain_name + ": " + (@success ? "Purchased" : @message)
+    return @full_domain_name + ": " + @message
   end
 end
