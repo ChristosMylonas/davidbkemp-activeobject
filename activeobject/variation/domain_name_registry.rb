071a0bd6ea87b51f564c5311e0c01d19b0b7750b
davidbkemp <davidbkemp@0f78eaf1-5652-0410-a00d-6df5d22118a9>
2008-07-19 11:58:59 +0000

    Minor improvements

--- activeobject/variation/domain_name_registry.rb
+++ activeobject/variation/domain_name_registry.rb
@@ -1,15 +1,16 @@
 class DomainNameRegistry
+  @@global_registry = []
   def initialize(domain_space)
     @domain_space = domain_space
-    @registry = []
   end
   def buy_domain_name(domain_name)
+    full_domain_name = "#{domain_name}.#{@domain_space}"
     sleep 2
-    if (@registry.include?(domain_name))
-      return PurchaseResult.new("#{domain_name}.#{@domain_space}", false, "Not Available")
+    if (@@global_registry.include?(full_domain_name))
+      return PurchaseResult.new(full_domain_name, false, "Not Available")
     end
-    @registry << domain_name
-    return PurchaseResult.new("#{domain_name}.#{@domain_space}", true, "Purchased") 
+    @@global_registry << full_domain_name
+    return PurchaseResult.new(full_domain_name, true, "Purchased") 
   end
 end
 
