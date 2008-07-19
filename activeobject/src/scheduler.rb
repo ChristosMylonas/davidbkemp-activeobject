31a091dcb8033d88df7be7498cb4526835ac8e03
davidbkemp <davidbkemp@0f78eaf1-5652-0410-a00d-6df5d22118a9>
2008-07-19 06:19:22 +0000

    Adding some documentation.

--- activeobject/src/scheduler.rb
+++ activeobject/src/scheduler.rb
@@ -13,3 +13,8 @@ class Scheduler
     @activation_queue.enqueue(item)
   end
 end
+
+# Could be made more general by having the activiation queue injected into it.
+# Could be made more flexible by having the target "servent" object injected and passing
+# the servent as a parameter to invocation of "call".  This would allow multiple servents
+# per activation queue (e.g. a domain name registry allowing more than one but still limited number of connections.)
