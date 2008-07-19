019c71d18abdbbd967afc9c1915c2cf6f9f95512
davidbkemp <davidbkemp@0f78eaf1-5652-0410-a00d-6df5d22118a9>
2008-07-19 03:18:54 +0000

    Now using multiple clients.

--- trunk/activeobject/src/active_domain_name_registry.rb
+++ trunk/activeobject/src/active_domain_name_registry.rb
@@ -1,10 +1,22 @@
 require "src/domain_name_registry_proxy"
 
-com = DomainNameRegistryProxy.new("com")
-com_au = DomainNameRegistryProxy.new("com.au")
-co_nz = DomainNameRegistryProxy.new("co.nz")
-com_result = com.buy_domain_name("foo")
-com_au_result = com_au.buy_domain_name("foo")
-co_nz_result = co_nz.buy_domain_name("foo")
-puts com_result.to_s + ", " + com_au_result.to_s + ", " + co_nz_result.to_s
-puts com.buy_domain_name("foo").to_s
+class DomainNameReseller
+  def initialize
+    @com = DomainNameRegistryProxy.new("com")
+    @com_au = DomainNameRegistryProxy.new("com.au")
+    @co_nz = DomainNameRegistryProxy.new("co.nz")
+  end
+  def buy_across_all_spaces(client_name, domain_name)
+    com_result = @com.buy_domain_name(domain_name)
+    com_au_result = @com_au.buy_domain_name(domain_name)
+    co_nz_result = @co_nz.buy_domain_name(domain_name)
+    puts client_name + ": " + com_result.to_s + ", " + com_au_result.to_s + ", " + co_nz_result.to_s
+  end
+end
+
+
+domain_name_reseller = DomainNameReseller.new
+client1 = Thread.new {domain_name_reseller.buy_across_all_spaces("A", "foo")}
+client2 = Thread.new {domain_name_reseller.buy_across_all_spaces("B", "foo")}
+client1.join
+client2.join
